d32046db73d7286c5ba85433f9ab8b25095a27d5
Natalia-Run <70914248+Natalia-Run@users.noreply.github.com>
2020-12-16 12:04:24 +0500

    Добавлен проект Visual Studio

---
+++ ConsoleApplication69/ConsoleApplication69/ConsoleApplication69.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+int main()
+{
+    int i, n;
+    time_t t;
+    setlocale(LC_ALL, "Russian");
+    time(&t);
+    srand((unsigned int)t);
+    cout << "Введите количество бочонков, которое будет вытащено: ";
+    cin >> n;
+
+    for (i = 1; i <= n; i++)
+    {
+        cout << rand() % 100 + 1 << endl;
+    }
+    return 0;
+}
